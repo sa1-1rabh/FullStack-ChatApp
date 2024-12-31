466160a32551b3423e67e9f589e2bbd3fec5f2ba
EXPERTGMR <111639654+EXPERTGMR@users.noreply.github.com>
2024-12-31 21:22:32 +0530

    minor fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 if (process.env.NODE_ENV == "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (res, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
